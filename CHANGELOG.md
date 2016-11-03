f8f23e814da46351568f0f18a9d65f007d40b34f
Javier Chavez <jachavez6@gmail.com>
2016-11-03 00:19:03 -0400

    Add CHANGELOG.md and rewrite tmdb.py to use classes
    Simplify the calls to the TMDB API by writing a wrapper using classes
    and objects.

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com) and this
+project adheres to [Semantic Versioning](http://semverg.org).
+
+## 0.0.1 - 2016
+### Added
+-
+-
+-
+-
--- tmdb.py
+++ tmdb.py
@@ -3,47 +3,63 @@ import requests
 
 tmdb_api = os.environ.get('TMDB_API_KEY')
 
-'''
-The movie information need is:
-1. Country
-2. Year
-3. Length
-4. Director
-5. Rating
-6. Distributor
-7. Language
-'''
+
+class TMDB(object):
+    def __init__(self):
+        self.api = tmdb_api
+        self.base_uri = 'https://api.themoviedb.org/3/'
 
 
-def search_tmdb(movie):
-    url = 'https://api.themoviedb.org/3/search/movie'
-    payload = {'api_key': tmdb_api, 'query': movie}
-    r = requests.get(url, data=payload)
-    results = r.json()['results']
-    movie_id = results[0]['id']
-    return movie_id
+class Search(TMDB):
+    def search(self, movie):
+        endpoint = 'search/movie'
+        payload = {'api_key': self.api, 'query': movie}
+        r = requests.get(self.base_uri + endpoint, data=payload)
+        results = r.json()['results']
+        first_three_movies = []
+        for result in results[:3]:
+            film = {}
+            film['title'] = result['title']
+            film['id'] = result['id']
+            first_three_movies.append(film)
+        return first_three_movies
 
 
-def get_details(movie_id):
-    url = 'https://api.themoviedb.org/3/movie/{}'.format(movie_id)
-    payload = {'api_key': tmdb_api}
-    r = requests.get(url, data=payload)
-    info = r.json()
-    title = info['original_title']
-    release = info['release_date']
-    print(title)
-    print(release)
-    return title, release
+class Movie(TMDB):
+    def __init__(self, movie_id):
+        super().__init__()
+        self.movie_id = movie_id
+        self.endpoints = {'details': 'movie/{}'.format(movie_id),
+                          'credits': 'movie/{}/credits'.format(movie_id)}
+        self.payload = {'api_key': self.api}
 
+    def details(self):
+        '''Returns the primary information about a movie.
 
-def get_credits(movie_id):
-    url = 'https://api.themoviedb.org/3/movie/{}/credits'.format(movie_id)
-    payload = {'api_key': tmdb_api}
-    r = requests.get(url, data=payload)
-    crew = r.json()['crew']
-    for i, member in enumerate(crew):
-        if member['job'] == 'Director':
-            director = member['name']
-            break
-    print(director)
-    return director
+        Contains the movie's country, year, length, and language.
+
+        Arguments:
+            none
+        Returns:
+            info = A dictionary.
+        '''
+        endpoint = self.endpoints['details']
+        r = requests.get(self.base_uri + endpoint, data=self.payload)
+        info = r.json()
+        return info
+
+    def credits(self):
+        '''Returns the cast and crew information about a movie.
+
+        Contains the director of the movie.
+
+        Arguments:
+            none
+        Returns:
+            cast = The cast of the movie.
+            crew = The crew of the movie.
+        '''
+        endpoint = self.endpoints['credits']
+        r = requests.get(self.base_uri + endpoint, data=self.payload)
+        cast, crew = r.json()['cast'], r.json()['crew']
+        return cast, crew
